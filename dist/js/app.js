1694749ec98bb1fdabfc6aa69aa6e0134811eb03
Fauztdinov Damir <fauztdinov@gmail.com>
2025-08-26 18:55:28 +0500

    all

--- dist/js/app.js
+++ dist/js/app.js
@@ -396,6 +396,93 @@ document.addEventListener("DOMContentLoaded", () => {
    });
 });
 
+
+
+/*------------------------------
+Popups
+---------------------------*/
+document.addEventListener("DOMContentLoaded", function () {
+   const openButtons = document.querySelectorAll('.open-popup');
+
+   if (openButtons && openButtons.length > 0) {
+      openButtons.forEach(button => {
+         if (!button) return;
+
+         button.addEventListener('click', function (e) {
+            if (this.tagName.toLowerCase() === 'a') {
+               e.preventDefault();
+            }
+
+            const popupId = this.dataset.popup;
+            if (!popupId) return;
+
+            const popup = document.getElementById(popupId);
+            if (popup) {
+               popup.classList.add('show');
+               document.body.style.overflow = 'hidden';
+            }
+         });
+      });
+   }
+
+   document.addEventListener('click', function (e) {
+      const openPopup = document.querySelector('.fv-popup.show');
+
+      if (!openPopup) return;
+
+      const isCloseBtn = e.target.closest('.fv-popup__close');
+      const isInsideBody = e.target.closest('.fv-popup__body');
+      const isPopupArea = e.target.closest('.fv-popup');
+
+      if (isCloseBtn || (!isInsideBody && isPopupArea)) {
+         openPopup.classList.remove('show');
+         document.body.style.overflow = '';
+      }
+   });
+});
+
+/*---------------------------------------------------------------------------
+Маска номера
+---------------------------------------------------------------------------*/
+window.addEventListener("DOMContentLoaded", function () {
+   [].forEach.call(document.querySelectorAll('input.tel-mask'), function (input) {
+      var keyCode;
+      function mask(event) {
+         event.keyCode && (keyCode = event.keyCode);
+         var pos = this.selectionStart;
+         if (pos < 3) event.preventDefault();
+         var matrix = "+7 (___) ___ ____",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            new_value = matrix.replace(/[_\d]/g, function (a) {
+               return i < val.length ? val.charAt(i++) : a
+            });
+         i = new_value.indexOf("_");
+         if (i != -1) {
+            i < 5 && (i = 3);
+            new_value = new_value.slice(0, i)
+         }
+         var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+            function (a) {
+               return "\\d{1," + a.length + "}"
+            }).replace(/[+()]/g, "\\$&");
+         reg = new RegExp("^" + reg + "$");
+         if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+            this.value = new_value;
+         }
+         if (event.type == "blur" && this.value.length < 5) {
+            this.value = "";
+         }
+      }
+
+      input.addEventListener("input", mask, false);
+      input.addEventListener("focus", mask, false);
+      input.addEventListener("blur", mask, false);
+      input.addEventListener("keydown", mask, false);
+   });
+});
+
 })();
 
 /******/ })()
